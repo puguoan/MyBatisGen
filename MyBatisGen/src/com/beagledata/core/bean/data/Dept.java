8047d5bddf0f35b0b4b89b45f87190543ea8ebfb
ssHss <ssHss@ssHss-PC>
2016-02-22 17:19:09 +0800

    //第一次提交代码 - Mybatis生成器

---
+++ MyBatisGen/src/com/beagledata/core/bean/data/Dept.java
@@ -0,0 +1,75 @@
+package com.beagledata.core.bean.data;
+
+import java.util.Date;
+
+public class Dept {
+    private Integer id;
+
+    private String name;
+
+    private String desc;
+
+    private Integer pid;
+
+    private Date createTime;
+
+    private Boolean isDeleted;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc == null ? null : desc.trim();
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
